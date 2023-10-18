4e7853ed22079693bfac595a17f0aad5d190e2dd
Pantosx2 <pantosxpantos@gmail.com>
2023-10-18 15:55:05 +0300

    Added a text file with a discritpion of what we did for each challenge, stil no progress in tier3

--- script15.c
+++ script15.c
@@ -8,9 +8,5 @@ void setup_tier2()
 
     long int *p = mmap((void *)(long)0x6042000, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
     *p = getpid();
-    void *p1 = dlopen("./tier3.so", 2);
-}
-
-void authorize_tier3()
-{
+    void *authorize_tier3 = dlopen("./tier3.so", 2);
 }
